bbe8b12b6ef2dbaeab9057b127f5c4e9f2d54986
Camila Lagos <118062775+camilagos@users.noreply.github.com>
2025-05-27 10:20:11 -0400

    Prueba

--- eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/tutorial/eurekaservice/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.eureka_service;
+package com.tutorial.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-service/src/test/java/com/tutorial/eurekaservice/EurekaServiceApplicationTests.java
+++ eureka-service/src/test/java/com/tutorial/eurekaservice/EurekaServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eureka_service;
+package com.tutorial.eurekaservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
