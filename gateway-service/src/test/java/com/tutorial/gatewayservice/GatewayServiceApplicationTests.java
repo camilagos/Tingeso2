235977f8a8c16c9c0f9196e7d23489e5a3f0e81c
Camila Lagos <118062775+camilagos@users.noreply.github.com>
2025-05-27 10:38:20 -0400

    Gateway

--- gateway-service/src/test/java/com/tutorial/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/tutorial/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tutorial.eurekaservice;
+package com.tutorial.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServiceApplicationTests {
+class GatewayServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
